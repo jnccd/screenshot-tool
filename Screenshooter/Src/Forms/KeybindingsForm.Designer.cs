6a18fd1140633809dcafdbe0561b7b0177fe1587
niklasCarstensen <niklas.carstensen@gmx.de>
2020-05-24 22:51:18 +0200

    Add icon to KeybindingsForm

--- Screenshooter/Src/Forms/KeybindingsForm.Designer.cs
+++ Screenshooter/Src/Forms/KeybindingsForm.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(KeybindingsForm));
             this.ComboBoxSpeicalInstant = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
@@ -132,6 +133,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.ComboBoxSpeicalInstant);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MaximumSize = new System.Drawing.Size(400, 150);
             this.MinimumSize = new System.Drawing.Size(400, 150);
